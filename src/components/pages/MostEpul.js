6838104c708d1a883b51b4f2deee9d6bbb847236
vinceagocs <64377515+agocsvince@users.noreply.github.com>
2021-12-15 13:04:56 +0100

    build

--- src/components/pages/MostEpul.js
+++ src/components/pages/MostEpul.js
@@ -1,36 +1,38 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { MostEpulContext } from '../../MostEpulContext';
 
 
 const MostEpul = props => {
+    const [state, setstate] = useState([])
     const { mostEpuls, isLoaded } = useContext(MostEpulContext);
 
-    if (isLoaded) {
-        return (
-        <body>
-            <main>
-                <section id="wip" className="py-1">
-                    <div className="text ml-4 mt-2">
-                        <h4 className="mb-05"><strong className="slash">\</strong> Referenciák</h4>
-                        <h2 className="mb-2">Elkészült munkáink</h2>
-                    </div>
-                    <div className="images grid m-4">
-                        {mostEpuls.map(mostEpul => (
-                            <Link className="image" to={"/most-epul/" + mostEpul.subtitle} key={mostEpul.id}>
-                                <img src={mostEpul.gallery[0].url} alt="" className="mb-05"/>
-                                <h3 className="mb-1">{mostEpul.title}</h3>
-                            </Link>
-                        ))}
-                    </div>
-                </section>
-            </main>
-        </body>
-        )
-    } else {
-        return 
-    }
-    
+    useEffect(() => {
+        if (isLoaded) {
+            setstate(mostEpuls)
+        }
+    }, [isLoaded,mostEpuls])
+
+    return (
+    <body>
+        <main>
+            <section id="wip" className="py-1">
+                <div className="text ml-4 mt-2">
+                    <h4 className="mb-05"><strong className="slash">\</strong> Referenciák</h4>
+                    <h2 className="mb-2">Elkészült munkáink</h2>
+                </div>
+                <div className="images grid m-4">
+                    {state.map(mostEpul => (
+                        <Link className="image" to={"/most-epul/" + mostEpul.subtitle} key={mostEpul.id}>
+                            <img src={mostEpul.gallery[0].url} alt="" className="mb-05"/>
+                            <h3 className="mb-1">{mostEpul.title}</h3>
+                        </Link>
+                    ))}
+                </div>
+            </section>
+        </main>
+    </body>
+    )
 }
 
 export default MostEpul;
--- src/components/pages/References.js
+++ src/components/pages/References.js
@@ -1,34 +1,39 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ReferenceContext } from '../../ReferenceContext';
 
 const References = props => {
+    const [state, setstate] = useState([])
     const { references, isLoaded } = useContext(ReferenceContext);
 
-    if (isLoaded) {
-        return (
-            <body>
-                <main>
-                    <section id="wip" className="py-1">
-                        <div className="text ml-4 mt-2">
-                            <h4 className="mb-05"><strong className="slash">\</strong> Referenciák</h4>
-                            <h2 className="mb-2">Elkészült munkáink</h2>
-                        </div>
-                        <div className="images grid m-4">
-                            {references.map(reference => (
-                                <Link className="image" to={"/referenciak/" + reference.subtitle} key={reference.id}>
-                                    <img src={reference.gallery[0].url} alt="" className="mb-05"/>
-                                    <h3 className="mb-1">{reference.title}</h3>
-                                </Link>
-                            ))}
-                        </div>
-                    </section>
-                </main>
-            </body>
-        )
-    } else {
-        return
-    }
+    useEffect(() => {
+        if (isLoaded) {
+            setstate(references)
+        }
+    }, [isLoaded,references])
+
+    
+    return (
+        <body>
+            <main>
+                <section id="wip" className="py-1">
+                    <div className="text ml-4 mt-2">
+                        <h4 className="mb-05"><strong className="slash">\</strong> Referenciák</h4>
+                        <h2 className="mb-2">Elkészült munkáink</h2>
+                    </div>
+                    <div className="images grid m-4">
+                        {state.map(reference => (
+                            <Link className="image" to={"/referenciak/" + reference.subtitle} key={reference.id}>
+                                <img src={reference.gallery[0].url} alt="" className="mb-05"/>
+                                <h3 className="mb-1">{reference.title}</h3>
+                            </Link>
+                        ))}
+                    </div>
+                </section>
+            </main>
+        </body>
+    )
+    
     
 }
 
