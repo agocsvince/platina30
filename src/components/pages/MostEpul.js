347e0716cb275ecf5226ca212493a56b8a166191
vinceagocs <64377515+agocsvince@users.noreply.github.com>
2021-12-15 12:51:42 +0100

    optimize pages

--- src/components/pages/MostEpul.js
+++ src/components/pages/MostEpul.js
@@ -8,25 +8,27 @@ const MostEpul = props => {
 
     if (isLoaded) {
         return (
-            <body>
-                <main>
-                    <section id="wip" className="py-1">
-                        <div className="text ml-4 mt-2">
-                            <h4 className="mb-05"><strong className="slash">\</strong> Referenciák</h4>
-                            <h2 className="mb-2">Elkészült munkáink</h2>
-                        </div>
-                        <div className="images grid m-4">
-                            {mostEpuls.map(mostEpul => (
-                                <Link className="image" to={"/most-epul/" + mostEpul.subtitle} key={mostEpul.id}>
-                                    <img src={mostEpul.gallery[0].url} alt="" className="mb-05"/>
-                                    <h3 className="mb-1">{mostEpul.title}</h3>
-                                </Link>
-                            ))}
-                        </div>
-                    </section>
-                </main>
-            </body>
+        <body>
+            <main>
+                <section id="wip" className="py-1">
+                    <div className="text ml-4 mt-2">
+                        <h4 className="mb-05"><strong className="slash">\</strong> Referenciák</h4>
+                        <h2 className="mb-2">Elkészült munkáink</h2>
+                    </div>
+                    <div className="images grid m-4">
+                        {mostEpuls.map(mostEpul => (
+                            <Link className="image" to={"/most-epul/" + mostEpul.subtitle} key={mostEpul.id}>
+                                <img src={mostEpul.gallery[0].url} alt="" className="mb-05"/>
+                                <h3 className="mb-1">{mostEpul.title}</h3>
+                            </Link>
+                        ))}
+                    </div>
+                </section>
+            </main>
+        </body>
         )
+    } else {
+        return 
     }
     
 }
