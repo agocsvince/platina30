d4296cc3bf9eb136e807bb787b12cf913c3cd4c7
vinceagocs <64377515+agocsvince@users.noreply.github.com>
2021-12-15 11:04:50 +0100

    initial

--- src/components/pages/MostEpul.js
+++ src/components/pages/MostEpul.js
@@ -4,28 +4,31 @@ import { MostEpulContext } from '../../MostEpulContext';
 
 
 const MostEpul = props => {
-    const { mostEpuls } = useContext(MostEpulContext);
+    const { mostEpuls, isLoaded } = useContext(MostEpulContext);
 
-    return (
-        <body>
-            <main>
-                <section id="wip" className="py-1">
-                    <div className="text ml-4 mt-2">
-                        <h4 className="mb-05"><strong className="slash">\</strong> Referenciák</h4>
-                        <h2 className="mb-2">Elkészült munkáink</h2>
-                    </div>
-                    <div className="images grid m-4">
-                        {mostEpuls.map(mostEpul => (
-                            <Link className="image" to={"/most-epul/" + mostEpul.subtitle} key={mostEpul.id}>
-                                <img src={mostEpul.gallery[0].url} alt="" className="mb-05"/>
-                                <h3 className="mb-1">{mostEpul.title}</h3>
-                            </Link>
-                        ))}
-                    </div>
-                </section>
-            </main>
-        </body>
-    )
+    if (isLoaded) {
+        return (
+            <body>
+                <main>
+                    <section id="wip" className="py-1">
+                        <div className="text ml-4 mt-2">
+                            <h4 className="mb-05"><strong className="slash">\</strong> Referenciák</h4>
+                            <h2 className="mb-2">Elkészült munkáink</h2>
+                        </div>
+                        <div className="images grid m-4">
+                            {mostEpuls.map(mostEpul => (
+                                <Link className="image" to={"/most-epul/" + mostEpul.subtitle} key={mostEpul.id}>
+                                    <img src={mostEpul.gallery[0].url} alt="" className="mb-05"/>
+                                    <h3 className="mb-1">{mostEpul.title}</h3>
+                                </Link>
+                            ))}
+                        </div>
+                    </section>
+                </main>
+            </body>
+        )
+    }
+    
 }
 
 export default MostEpul;
--- src/components/pages/References.js
+++ src/components/pages/References.js
@@ -3,28 +3,31 @@ import { Link } from 'react-router-dom';
 import { ReferenceContext } from '../../ReferenceContext';
 
 const References = props => {
-    const {references} = useContext(ReferenceContext);
+    const { references, isLoaded } = useContext(ReferenceContext);
 
-    return (
-        <body>
-            <main>
-                <section id="wip" className="py-1">
-                    <div className="text ml-4 mt-2">
-                        <h4 className="mb-05"><strong className="slash">\</strong> Referenciák</h4>
-                        <h2 className="mb-2">Elkészült munkáink</h2>
-                    </div>
-                    <div className="images grid m-4">
-                        {references.map(reference => (
-                            <Link className="image" to={"/referenciak/" + reference.subtitle} key={reference.id}>
-                                <img src={reference.gallery[0].url} alt="" className="mb-05"/>
-                                <h3 className="mb-1">{reference.title}</h3>
-                            </Link>
-                        ))}
-                    </div>
-                </section>
-            </main>
-        </body>
-    )
+    if (isLoaded) {
+        return (
+            <body>
+                <main>
+                    <section id="wip" className="py-1">
+                        <div className="text ml-4 mt-2">
+                            <h4 className="mb-05"><strong className="slash">\</strong> Referenciák</h4>
+                            <h2 className="mb-2">Elkészült munkáink</h2>
+                        </div>
+                        <div className="images grid m-4">
+                            {references.map(reference => (
+                                <Link className="image" to={"/referenciak/" + reference.subtitle} key={reference.id}>
+                                    <img src={reference.gallery[0].url} alt="" className="mb-05"/>
+                                    <h3 className="mb-1">{reference.title}</h3>
+                                </Link>
+                            ))}
+                        </div>
+                    </section>
+                </main>
+            </body>
+        )
+    }
+    
 }
 
 export default References;
